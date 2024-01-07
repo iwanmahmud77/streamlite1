3d4978e1de7d58b87c40c4dbe923f7348fedfe4a
iwanmahmud77 <52195525+iwanmahmud77@users.noreply.github.com>
2024-01-08 07:24:06 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,47 +3,12 @@ import pickle
 import pandas as pd
 import streamlit as st 
 
-from PIL import Image
-
-
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
 
-def welcome():
-    return "Welcome All"
 
 def predict_note_authentication(variance,skewness,curtosis,entropy):
-    
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
-   
     prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
-    print(prediction)
-    return prediction
-
 
 
 def main():
@@ -66,8 +31,3 @@ def main():
         st.text("Lets Learn Python")
         st.text("Built with Streamlit")
 
-if __name__=='__main__':
-    main()
-    
-    
-    
